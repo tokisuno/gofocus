76487c1cd827295bd1465b00b218e9c91d142eab
tokisuno <lucaswmei@gmail.com>
2025-03-14 10:47:29 -0400

    works pretty well now! inbetween scenes are missing shadows

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 const STUDY_MINUTES int = 50 * 60
 const BREAK_MINUTES int = 10 * 60
 
-const screenW = int32(800)
-const screenH = int32(600)
+const screenW = int32(900)
+const screenH = int32(300)
 
 type GameState int
 
@@ -38,78 +38,124 @@ func newHms(seconds int) *hms {
 }
 
 func main() {
-	rl.InitWindow(800, 600, "Raylib Example")
+	rl.InitWindow(screenW, screenH, "Raylib Example")
+
 	defer rl.CloseWindow()
+
+	fontSize := 140
+	font := rl.LoadFontEx("assets/JetBrainsMono-VariableFont_wght.ttf", int32(fontSize), nil)
 
 	studyTime := STUDY_MINUTES
 	breakTime := BREAK_MINUTES
+
 	studyTimerHms := newHms(studyTime)
 	breakTimerHms := newHms(breakTime)
+
+	studyText := fmt.Sprintf("%.2d:%.2d:%.2d", studyTimerHms.hrs, studyTimerHms.mins, studyTimerHms.secs)
+	breakText := fmt.Sprintf("%.2d:%.2d:%.2d", breakTimerHms.hrs, breakTimerHms.mins, breakTimerHms.secs)
 
 	var currentScreen GameState
 	currentScreen = waitSTUDY
 
+	rl.SetExitKey(rl.KeyNull)
+
 	frames := 0
+
+	fontPosition := rl.NewVector2(100, 100)
+
+	studyTextSize := rl.MeasureTextEx(font, studyText, float32(fontSize), 0)
+	breakTextSize := rl.MeasureTextEx(font, studyText, float32(fontSize), 0)
+
+	studyTextPosition := rl.NewVector2(float32(screenW)/2 - (studyTextSize.X/2), 90)
+	studyShadowPosition1 := studyTextPosition
+	studyShadowPosition1.X += 2
+	studyShadowPosition1.Y += 5
+
+	studyShadowPosition2 := studyShadowPosition1
+	studyShadowPosition2.X += 2
+	studyShadowPosition2.Y += 5
+
+	breakTextPosition := rl.NewVector2(float32(screenW)/2 - (breakTextSize.X/2), 90)
+	breakShadowPosition1 := breakTextPosition
+	breakShadowPosition1.X += 2
+	breakShadowPosition1.Y += 5
+
+	breakShadowPosition2 := breakShadowPosition1
+	breakShadowPosition2.X += 2
+	breakShadowPosition2.Y += 5
+
+	rl.SetTextureFilter(font.Texture, rl.TextureFilterNearest)
+
+	bgColour := rl.GetColor(0x282828ff)
+	textColour := rl.GetColor(0xfbf1c7ff)
+	textShadowColour1 := rl.GetColor(0xa89984ff)
+	textShadowColour2 := rl.GetColor(0x655c54ff)
 
 	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
 		switch currentScreen {
 		case waitSTUDY:
-			if rl.IsKeyPressed(rl.KeyEnter) {
+			if rl.IsKeyDown(rl.KeyLeftControl) && rl.IsKeyPressed(rl.KeyY) {
 				currentScreen = STUDY
 			}
 		case STUDY:
 			frames++
-			if frames > 60 {
+			if frames == 60 {
 				studyTime--
 				studyTimerHms = newHms(studyTime)
+				studyText = fmt.Sprintf("%.2d:%.2d:%.2d", studyTimerHms.hrs, studyTimerHms.mins, studyTimerHms.secs)
 				frames = 0
 			}
-			if rl.IsKeyPressed(rl.KeyEnter) {
+			if rl.IsKeyDown(rl.KeyLeftControl) && rl.IsKeyPressed(rl.KeyY) {
 				currentScreen = waitBREAK
 				studyTime = STUDY_MINUTES
 				studyTimerHms = newHms(studyTime)
+				studyText = fmt.Sprintf("%.2d:%.2d:%.2d", studyTimerHms.hrs, studyTimerHms.mins, studyTimerHms.secs)
 			}
 		case waitBREAK:
-			if rl.IsKeyPressed(rl.KeyEnter) {
+			if rl.IsKeyDown(rl.KeyLeftControl) && rl.IsKeyPressed(rl.KeyY) {
 				currentScreen = BREAK
 			}
 		case BREAK:
 			frames++
-			if frames > 60 {
+			if frames == 60 {
 				breakTime--
 				breakTimerHms = newHms(breakTime)
 				frames = 0
+				breakText = fmt.Sprintf("%.2d:%.2d:%.2d", breakTimerHms.hrs, breakTimerHms.mins, breakTimerHms.secs)
 			}
-			if rl.IsKeyPressed(rl.KeyEnter) {
+			if rl.IsKeyDown(rl.KeyLeftControl) && rl.IsKeyPressed(rl.KeyY) {
 				currentScreen = waitSTUDY 
 				breakTime = BREAK_MINUTES 
 				breakTimerHms = newHms(breakTime)
+				breakText = fmt.Sprintf("%.2d:%.2d:%.2d", breakTimerHms.hrs, breakTimerHms.mins, breakTimerHms.secs)
 			}
 		}
 
 		rl.BeginDrawing()
-
-		rl.ClearBackground(rl.Black)
+		rl.ClearBackground(bgColour)
 		// rec := rl.NewRectangle(0, 0, float32(screenW), float32(screenH))
 
 		switch currentScreen {
 		case waitSTUDY:
-			rl.DrawText("Study time!", 100, 100, 20, rl.White)
+			rl.DrawTextEx(font, "Study time!", fontPosition, float32(fontSize), 0, textColour)
 		case STUDY:
-			rl.DrawText(
-				fmt.Sprintf("%.2d:%.2d:%.2d", studyTimerHms.hrs, studyTimerHms.mins, studyTimerHms.secs),
-				100, 100, 20, rl.White)
-
+			rl.DrawTextEx(font, studyText, studyShadowPosition2, float32(fontSize), 0, textShadowColour2)
+			rl.DrawTextEx(font, studyText, studyShadowPosition1, float32(fontSize), 0, textShadowColour1)
+			rl.DrawTextEx(font, studyText, studyTextPosition, float32(fontSize), 0, textColour)
 		case waitBREAK:
-			rl.DrawText("Break time!", 100, 100, 20, rl.White)
+			rl.DrawTextEx(font, "Break time!", fontPosition, float32(fontSize), 0, textColour)
 		case BREAK:
-			rl.DrawText(
-				fmt.Sprintf("%.2d:%.2d:%.2d", breakTimerHms.hrs, breakTimerHms.mins, breakTimerHms.secs),
-				100, 100, 20, rl.White)
+			rl.DrawTextEx(font, breakText, breakShadowPosition2, float32(fontSize), 0, textShadowColour2)
+			rl.DrawTextEx(font, breakText, breakShadowPosition1, float32(fontSize), 0, textShadowColour1)
+			rl.DrawTextEx(font, breakText, breakTextPosition, float32(fontSize), 0, textColour)
 		}	
 
 		rl.EndDrawing()
 	}
+
+	rl.UnloadFont(font)
+	rl.UnloadDroppedFiles()
+	rl.CloseWindow()
 }
