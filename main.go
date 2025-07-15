4049f19b977a1a22e576c163c009d327c86976b2
tokisuno <lucaswmei@gmail.com>
2025-07-15 16:24:10 -0400

    added session counter at the bottom; adjustable using vim keybinds

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 const STUDY_MINUTES int = 50 * 60
 const BREAK_MINUTES int = 10 * 60
+const SESSION_AMNT int = 4
 
 const screenW = int32(900)
 const screenH = int32(300)
@@ -37,14 +38,13 @@ func newHms(seconds int) *hms {
 	return &time
 }
 
-
 func main() {
 	rl.InitWindow(screenW, screenH, "GoFocus!")
 	rl.InitAudioDevice()
 
 	defer rl.CloseWindow()
 
-	fontSize := 120
+	fontSize := 100
 	font := rl.LoadFontEx("assets/JetBrainsMono-VariableFont_wght.ttf", int32(fontSize), nil)
 
 	studyTime := STUDY_MINUTES
@@ -65,6 +65,15 @@ func main() {
 	studyText := fmt.Sprintf("%.2d:%.2d:%.2d", studyTimerHms.hrs, studyTimerHms.mins, studyTimerHms.secs)
 	breakText := fmt.Sprintf("%.2d:%.2d:%.2d", breakTimerHms.hrs, breakTimerHms.mins, breakTimerHms.secs)
 
+	sessionCounter := 0
+	totalSessions := SESSION_AMNT
+
+	sessionFontSize := 60
+	sessionFont := rl.LoadFontEx("assets/JetBrainsMono-VariableFont_wght.ttf", int32(sessionFontSize), nil)
+	sessionText := fmt.Sprintf("Session %d/%d!", sessionCounter, totalSessions)
+	sessionTextSize := rl.MeasureTextEx(sessionFont, sessionText, float32(sessionFontSize), 0)
+	sessionTextPosition := rl.NewVector2(float32(screenW)/2-(sessionTextSize.X/2), (float32(screenH)-sessionTextSize.Y)-30)
+
 	var currentScreen GameState
 	currentScreen = waitSTUDY
 
@@ -74,7 +83,7 @@ func main() {
 
 	studySplash := "Study time!"
 	studySplashSize := rl.MeasureTextEx(font, studySplash, float32(fontSize), 0)
-	studySplashPosition := rl.NewVector2(float32(screenW)/2 - (studySplashSize.X/2), 90)
+	studySplashPosition := rl.NewVector2(float32(screenW)/2-(studySplashSize.X/2), 70)
 
 	studySplashPosition1 := studySplashPosition
 	studySplashPosition1.X += 2
@@ -86,7 +95,7 @@ func main() {
 
 	breakSplash := "Time to rest :3"
 	breakSplashSize := rl.MeasureTextEx(font, breakSplash, float32(fontSize), 0)
-	breakSplashPosition := rl.NewVector2(float32(screenW)/2 - (breakSplashSize.X/2), 90)
+	breakSplashPosition := rl.NewVector2(float32(screenW)/2-(breakSplashSize.X/2), 70)
 
 	breakSplashPosition1 := breakSplashPosition
 	breakSplashPosition1.X += 2
@@ -99,7 +108,7 @@ func main() {
 	studyTextSize := rl.MeasureTextEx(font, studyText, float32(fontSize), 0)
 	breakTextSize := rl.MeasureTextEx(font, studyText, float32(fontSize), 0)
 
-	studyTextPosition := rl.NewVector2(float32(screenW)/2 - (studyTextSize.X/2), 90)
+	studyTextPosition := rl.NewVector2(float32(screenW)/2-(studyTextSize.X/2), 70)
 	studyShadowPosition1 := studyTextPosition
 	studyShadowPosition1.X += 2
 	studyShadowPosition1.Y += 5
@@ -108,7 +117,7 @@ func main() {
 	studyShadowPosition2.X += 2
 	studyShadowPosition2.Y += 5
 
-	breakTextPosition := rl.NewVector2(float32(screenW)/2 - (breakTextSize.X/2), 90)
+	breakTextPosition := rl.NewVector2(float32(screenW)/2-(breakTextSize.X/2), 70)
 	breakShadowPosition1 := breakTextPosition
 	breakShadowPosition1.X += 2
 	breakShadowPosition1.Y += 5
@@ -119,14 +128,23 @@ func main() {
 
 	rl.SetTextureFilter(font.Texture, rl.TextureFilterNearest)
 
-	bgColour 	 := rl.GetColor(0x1d2021ff)
+	bgColour := rl.GetColor(0x1d2021ff)
 	textColour := rl.GetColor(0xebdbb2ff)
 	textShadowColour1 := rl.GetColor(0x504945ff)
 	textShadowColour2 := rl.GetColor(0x282828ff)
+	sessionTextColour := rl.GetColor(0xffff0045)
 
 	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
+		if rl.IsKeyDown(rl.KeyLeftControl) && rl.IsKeyPressed(rl.KeyA) {
+			sessionCounter++
+			sessionText = fmt.Sprintf("Session %d/%d!", sessionCounter, totalSessions)
+		}
+		if rl.IsKeyDown(rl.KeyLeftControl) && rl.IsKeyPressed(rl.KeyX) {
+			sessionCounter--
+			sessionText = fmt.Sprintf("Session %d/%d!", sessionCounter, totalSessions)
+		}
 		switch currentScreen {
 		case waitSTUDY:
 			if sound_played == false {
@@ -184,18 +202,22 @@ func main() {
 			rl.DrawTextEx(font, studySplash, studySplashPosition2, float32(fontSize), 0, textShadowColour2)
 			rl.DrawTextEx(font, studySplash, studySplashPosition1, float32(fontSize), 0, textShadowColour1)
 			rl.DrawTextEx(font, studySplash, studySplashPosition, float32(fontSize), 0, textColour)
+			rl.DrawTextEx(sessionFont, sessionText, sessionTextPosition, float32(sessionFontSize), 0, sessionTextColour)
 		case STUDY:
 			rl.DrawTextEx(font, studyText, studyShadowPosition2, float32(fontSize), 0, textShadowColour2)
 			rl.DrawTextEx(font, studyText, studyShadowPosition1, float32(fontSize), 0, textShadowColour1)
 			rl.DrawTextEx(font, studyText, studyTextPosition, float32(fontSize), 0, textColour)
+			rl.DrawTextEx(sessionFont, sessionText, sessionTextPosition, float32(sessionFontSize), 0, sessionTextColour)
 		case waitBREAK:
 			rl.DrawTextEx(font, breakSplash, breakSplashPosition2, float32(fontSize), 0, textShadowColour2)
 			rl.DrawTextEx(font, breakSplash, breakSplashPosition1, float32(fontSize), 0, textShadowColour1)
 			rl.DrawTextEx(font, breakSplash, breakSplashPosition, float32(fontSize), 0, textColour)
+			rl.DrawTextEx(sessionFont, sessionText, sessionTextPosition, float32(sessionFontSize), 0, sessionTextColour)
 		case BREAK:
 			rl.DrawTextEx(font, breakText, breakShadowPosition2, float32(fontSize), 0, textShadowColour2)
 			rl.DrawTextEx(font, breakText, breakShadowPosition1, float32(fontSize), 0, textShadowColour1)
 			rl.DrawTextEx(font, breakText, breakTextPosition, float32(fontSize), 0, textColour)
+			rl.DrawTextEx(sessionFont, sessionText, sessionTextPosition, float32(sessionFontSize), 0, sessionTextColour)
 		}
 
 		rl.EndDrawing()
